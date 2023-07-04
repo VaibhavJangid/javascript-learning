c0314aed4861530756cfbf2a1ef06854b3c8d112
VaibhavJangid <81412297+VaibhavJangid@users.noreply.github.com>
2023-07-04 17:55:13 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,27 @@
+let score = "Vaibhav"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a Number) but it shows type Number
+// true => 1, false=> 0
+
+let isLoggedIn = "Vaibhav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// ""(empty string) => false
+// "Vaibhav" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
