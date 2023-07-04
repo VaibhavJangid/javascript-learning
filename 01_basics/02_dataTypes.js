9016458f9935c3144ecee081138e088092b8366b
VaibhavJangid <81412297+VaibhavJangid@users.noreply.github.com>
2023-07-04 15:26:55 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert ( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code readability should be high
+
+console.log("Vaibhav")
+
+
+let name = "Viabhav"
+let age = "22"
+let isLoggedIn = false
+
+//  PRIMITIVE DATATYPE
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine => undefine
+// symbol => unique (majorly used in react in big projects)
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
