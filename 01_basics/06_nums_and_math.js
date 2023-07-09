d56d28dc3cab083970a50c6c50d8e4df581b7d52
VaibhavJangid <81412297+VaibhavJangid@users.noreply.github.com>
2023-07-09 09:21:05 +0000

    Number and Maths

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -16,6 +16,23 @@ const hundreds = 1000000
 
 // ******************* Maths ******************
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.6));
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.6)); // Most commonly used
+
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.sqrt(4));
+// console.log(Math.min(4, 6, 8, 1));
+// console.log(Math.max(4, 6, 8));
+
+console.log(Math.random()); // in .random value are between 0-1 like 0.51651, 0.251516
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
