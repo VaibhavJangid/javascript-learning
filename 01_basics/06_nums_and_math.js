90eb65b2b5044389e2198455318babc19c5a0518
VaibhavJangid <81412297+VaibhavJangid@users.noreply.github.com>
2023-07-09 05:26:17 +0000

    Number and Maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,21 @@
+const score = 400
+// console.log(score.typeof);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.2585
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ******************* Maths ******************
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
