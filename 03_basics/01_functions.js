3278230edec78d8308f77926a33981e1ab3b4f13
VaibhavJangid <81412297+VaibhavJangid@users.noreply.github.com>
2023-07-12 15:47:51 +0000

    Functions with objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,4 +36,37 @@ function loginUserMessage(username = "You"){
 }
 
 // console.log(loginUserMessage("Vaibhav"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val, val2, ...num1){
+    return num1
+}
+
+// above we have put "val1, val2" so now 100 and 200 values are now in these variables, if we remove them then the all the values will show up.
+
+// console.log(calculateCartPrice(100, 200, 300, 400, 500));
+
+const user = {
+    username: "Vaibhav",
+    price: 199
+}
+
+function handleobject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}.`);
+}
+
+// handleobject(user)
+handleobject({
+    username: "sam",
+    price: 399
+})
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
