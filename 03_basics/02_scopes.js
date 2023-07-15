63b962c45110138fb5178b2bbea40a6026c6693c
VaibhavJangid <81412297+VaibhavJangid@users.noreply.github.com>
2023-07-15 10:36:51 +0000

    Scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,4 +1,4 @@
-// "{}" This currly brace is used in defineing a object, but the same "{}" currly brace is also used in defineing a scope but it can be call scope if it is used with a "function or if/else statement" then it can be called "SCOPE".
+ // "{}" This currly brace is used in defineing a object, but the same "{}" currly brace is also used in defineing a scope but it can be call scope if it is used with a "function or if/else statement" then it can be called "SCOPE".
 
 // let a = 10
 // const b = 20
@@ -12,7 +12,7 @@
 
 // **** coding enviroment scope is different from browser scope
 
-let a = 300 // **** Global Scope ****
+// let a = 300 **** Global Scope ****
 
 if (true) {
     let a = 10
@@ -23,4 +23,48 @@ if (true) {
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+function one(){
+    const username = "Vaibhav"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+   
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "Viabhav"
+    if (username === "Vaibhav") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+
+    // console.log(website);
+}
+
+// console.log(username)
+
+// ****************** intersting *****************
+
+console.log((addone(5)));
+
+function addone(num){
+    return num + 1
+    
+}
+
+// in above case we our code is working properly when we "log" it before declearing the function.
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
+// But in this code our code showing error because of the hoisting problem which is occuring because we have given function in a variable.
