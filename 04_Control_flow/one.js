1de78099c48474d7d35ed1f9e935886b9bf13363
VaibhavJangid <81412297+VaibhavJangid@users.noreply.github.com>
2023-07-16 16:51:17 +0000

    Control flow

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,25 @@
+// if
+
+if (true){
+
+
+}
+// if there is "true" in boolean then the (for Ex) code from line 4-5 will be performed.
+
+if (false){
+
+
+}
+// if there is "false" in boolean then the (for Ex) code from line 10-11 will not be performed.
+
+// How can comparison can be happen.
+
+// operators
+// <, >, <=, >=, ==, !=, ===, !==
+
+const temperature = 40
+
+if ( temperature === 50 ){
+    console.log("temperature less than 50");
+}
+console.log("temperature is greater than 50");
