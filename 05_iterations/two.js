f0c14f2875cbe53190be30c192031ca1067eb893
VaibhavJangid <81412297+VaibhavJangid@users.noreply.github.com>
2023-07-22 15:34:58 +0000

    While and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,35 @@
+// mostly we use "for" loop.
+
+// while loop
+
+// simple structure of while loop
+// while (condition) {
+    
+// }
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['flash', 'batman', 'superman']
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+// do while loop
+
+// simple structure of do while loop
+// do {
+    
+// } while (condition);
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
