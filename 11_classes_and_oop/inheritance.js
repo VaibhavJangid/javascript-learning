e180b3f28dffbc0e8a9791a6f38cc789940c1370
Vaibhav Sharma <81412297+VaibhavJangid@users.noreply.github.com>
2023-08-31 15:54:57 +0000

    inheritance

---
+++ 11_classes_and_oop/inheritance.js
@@ -0,0 +1,34 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+// extends: It is use to coat a layer. like normal user and admin. It extends for admins.
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        // Super: prototype.username.call
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}.`);
+    }
+}
+
+const Vaibhav = new Teacher("Vaibhav", "vaibhav@mail.com", "123")
+
+// Vaibhav.addCourse()
+
+const Gojo = new User('Gojo')
+// Gojo.addCourse()
+// Gojo.logMe()
+// Vaibhav.logMe()
+
+console.log(Vaibhav instanceof User);
